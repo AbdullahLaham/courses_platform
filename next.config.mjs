754fc57ca5a56ae19f55ea026962ff511a5c8715
AbdullahLaham <abed26194@gmail.com>
2024-08-20 19:22:57 +0300

    ch

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "utfs.io"
+        ]
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default nextConfig;
